2f0ad271b9213ac87ed2b1ae1a8c0fd672e7c9b4
annie <ruthannie3@gmail.com>
2020-09-28 13:28:50 +0530

    [Annie]Added: Regex pattern to validate pin code

--- src/com/bridgelabz/pattern/pincode/Pincode.java
+++ src/com/bridgelabz/pattern/pincode/Pincode.java
@@ -6,9 +6,30 @@ import java.util.regex.Pattern;
 
 public class Pincode {
 
-   
+   public boolean checkPattern(String name) {
+	   boolean result = false;
+	   String pattern = "[0-9]{6}";
+	   
+	   // Create a Pattern object
+	   Pattern r = Pattern.compile(pattern);
+
+	   // Now create matcher object.
+	   Matcher m = r.matcher(name);
+	   if (m.find())
+		   	result = true;
+	   else 
+		   result = false;
+	   
+	   return result;
+   }
+	
 	
 	public static void main( String args[] ) {
-	   System.out.println("Welcome to PIN CODE validation simulator");
+	   Scanner sc = new Scanner(System.in);
+       Pincode obj = new Pincode();
+       System.out.println("Enter pin");
+       String value = sc.nextLine();
+       Boolean ans = obj.checkPattern(value);
+       System.out.println(ans);
    }
 }
