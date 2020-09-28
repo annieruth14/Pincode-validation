666ad3236fbf0f29767492d70ab9e80cbfc200ac
annie <ruthannie3@gmail.com>
2020-09-28 13:19:15 +0530

    [Annie]Added: Welcome message

---
+++ src/com/bridgelabz/pattern/pincode/Pincode.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.pattern.pincode;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Pincode {
+
+   
+	
+	public static void main( String args[] ) {
+	   System.out.println("Welcome to PIN CODE validation simulator");
+   }
+}
